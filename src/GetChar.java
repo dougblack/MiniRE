ad63e3010e08a8e69fa0615b12736f4d67889221
alazar <alazar.tamrat@gmail.com>
2012-11-07 20:43:44 -0500

    char reader

---
+++ src/GetChar.java
@@ -0,0 +1,25 @@
+// GetChar: fetch next char 
+import java.io.*;
+import java.nio.charset.Charset;
+public class GetChar { 
+   private Reader in; // internal file name for input stream
+   private BufferedReader reader;
+    
+   public GetChar (String filename) throws FileNotFoundException, IOException { 
+	   File file = new File(filename);
+	   reader=new BufferedReader(
+	   new InputStreamReader(
+		        new FileInputStream(file),
+		        Charset.forName("UTF-8")));
+   }
+
+   public char getNextChar() throws IOException {
+      char ch=(char)-1;//' '; // = ' ' to keep compiler happy
+      try {
+         ch = (char)reader.read();  
+      } catch (IOException e) {
+         System.out.println("char exception");
+      }
+      return ch;
+   }
+}
