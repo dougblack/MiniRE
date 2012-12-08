7a7eb2b64881e8956f6d79bfbee6e4b0a63c59cb
alazar <alazar@gmail.com>
2012-12-07 19:10:41 -0500

    untested evaluator

--- src/interpreter/Evaluator.java
+++ src/interpreter/Evaluator.java
@@ -1,5 +1,9 @@
 package interpreter;
 import java.util.*;
+import java.util.regex.*;
+
+import tokenizer.Token;
+import tokenizer.Tokenizer;
 
 
 public class Evaluator {
@@ -71,4 +75,52 @@ public class Evaluator {
 		
 		return freqstring;
 	}
+	
+	
+	public static String find(String regex, String filename){
+		
+		String matches="";
+		Tokenizer d = new Tokenizer(filename, "/home/alazar/git/MiniRE/src/interpreter/test.txt");
+		d.generateTokens();
+        ArrayList<Token> tokens = d.getTokens();
+        //
+        for (int i = 0; i < tokens.size(); i++) {
+            System.out.println(tokens.get(i).getId() + ": " +
+                tokens.get(i).getString());
+            matches+=tokens.get(i);
+        }
+		return matches;
+	}
+	
+	
+	public static String replace(String regex, String with, String filename1, String filename2){
+		
+		
+		String matched = "";
+		if(regex.toCharArray()[0] == '\''){ // this is a regexp
+			
+			regex = regex.substring(1,regex.length()-1);
+			regex.toCharArray()[0] = ' ';
+			regex.toCharArray()[regex.length()-1] = ' ';
+			regex = regex.trim();
+			
+			Pattern pattern = Pattern.compile(regex);
+			Matcher matcher = pattern.matcher(regex);
+			while(matcher.find()){
+				matched+=matcher.group(0);
+			}
+			if(matched.length()>0)
+				return regex.replace(matched, with);
+			return "";
+		}
+		
+		return matched;
+	}
+	
+	public static String print(String string){
+		
+		System.out.println(string);
+		return string;
+	}
+	
 }
