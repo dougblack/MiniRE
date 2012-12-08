b7098331fdab38c16c27e174d285df647b263459
alazar <alazar@gmail.com>
2012-12-07 20:32:41 -0500

    recursivereplace + javadoc comments

--- src/interpreter/Evaluator.java
+++ src/interpreter/Evaluator.java
@@ -9,24 +9,51 @@ import java.util.regex.*;
 import tokenizer.Token;
 import tokenizer.Tokenizer;
 
-
+/**
+ * 
+ * carries out final string operations on matched tokens
+ *
+ */
 public class Evaluator {
 
+	/**
+	 * evaluator for #
+	 * @param string
+	 * @return 
+	 */
 	public static String getStringLength(String string){
 		
 		return string.length()+"";
 	}
 	
+	/**
+	 * string to string list adaptor (space-separation=set)
+	 * @param a
+	 * @param b
+	 * @return 
+	 */
 	public static String inters(String a, String b){
 		
 		return inters(Arrays.asList(a.split(" ")), Arrays.asList(b.split(" ")));
 	}
 	
+	/**
+	 * string to string list adaptor
+	 * @param a
+	 * @param b
+	 * @return
+	 */
 	public static String union(String a, String b){
 		
 		return union(Arrays.asList(a.split(" ")), Arrays.asList(b.split(" ")));
 	}
 	
+	/**
+	 * evaluator intersection of string lists
+	 * @param list1
+	 * @param list2
+	 * @return
+	 */
 	public static String inters(List<String> list1, List<String> list2){
 		
 		List<String> list = new ArrayList<String>();
@@ -41,6 +68,12 @@ public class Evaluator {
         
 	}
 	
+	/**
+	 * evaluator for union of string lists
+	 * @param list1
+	 * @param list2
+	 * @return
+	 */
 	public static String union(List<String> list1, List<String> list2){
 		
 		  Set<String> set = new HashSet<String>();
@@ -53,10 +86,20 @@ public class Evaluator {
 	        
 	}
 	
+	/**
+	 * string to string list adaptor
+	 * @param string
+	 * @return
+	 */
 	public static String maxfreqstring(String string){
 		return maxfreqstring(string.split(" "));
 	}
 	
+	/**
+	 * evaluator for maximum frequency of a string in a list
+	 * @param string
+	 * @return
+	 */
 	public static String maxfreqstring(String string[]){
 		
 		String freqstring="";
@@ -81,6 +124,12 @@ public class Evaluator {
 	}
 	
 	
+	/**
+	 * evaluator for finding regexp in a file
+	 * @param regex
+	 * @param filename
+	 * @return
+	 */
 	public static String find(String regex, String filename){
 		
 		if(regex.toCharArray()[0] == '\''){ // this is a regexp
@@ -99,12 +148,22 @@ public class Evaluator {
 		return "";
 	}
 	
-	
+	/**
+	 * evaluator for finding and replacing regexp matches in filename1 and writing to filename2
+	 * @param regex
+	 * @param with
+	 * @param filename1
+	 * @param filename2
+	 * @return
+	 * @throws IOException
+	 */
 	public static String replace(String regex, String with, String filename1, String filename2) throws IOException{
-		
 		
 		String matched = "";
 		String i_to_replace[] = find(regex, filename1).split(" ");
+		if(i_to_replace.length==0)
+			return -1+"";
+		
 		String in = "";
 		regex = regex.substring(1,regex.length()-1);
 		FileInputStream stream = new FileInputStream(new File(filename1));
@@ -128,6 +187,30 @@ public class Evaluator {
 		return in;
 	}
 	
+	
+	/**
+	 * evaluator for recursive replacing of regexps in output files
+	 * @param regex
+	 * @param with
+	 * @param filename1
+	 * @param filename2
+	 * @return
+	 * @throws IOException
+	 */
+	public static String recursivereplace(String regex, String with, String filename1, String filename2) throws IOException{
+		
+		String rep_stat = replace(regex, with, filename1, filename2);
+		while(rep_stat.compareTo("-1")!=0){
+			rep_stat = replace(regex,with,filename2,filename2);
+		}
+		return "0";
+	}
+	
+	/**
+	 * evaluator for printing a string
+	 * @param string
+	 * @return
+	 */
 	public static String print(String string){
 		
 		System.out.println(string);
